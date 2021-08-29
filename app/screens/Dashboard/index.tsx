87bd18c1594b21b3a37d6f1ebf6dcc1635545c0c
Luis Ramirez <luis@codinglabs.dev>
2021-08-29 00:47:24 -0300

    feat(Dashboard): Create top bar

--- app/screens/Dashboard/index.tsx
+++ app/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
+import Container from 'app/components/ui/Container';
 import React from 'react';
 
-import { Container, Text } from './styles';
+import * as S from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Container>
-      <Text>Test</Text>
-    </Container>
+    <S.Container>
+      <S.Header>
+        <Container>
+          <S.InfoBar>
+            <S.User>
+              <S.UserPhoto
+                source={{
+                  uri: 'https://avatars.githubusercontent.com/u/48620427?v=4',
+                }}
+              />
+              <S.UserInfo>
+                <S.UserGreetings>Olá,</S.UserGreetings>
+                <S.UserName>Luis</S.UserName>
+              </S.UserInfo>
+            </S.User>
+            <S.Icon />
+          </S.InfoBar>
+        </Container>
+      </S.Header>
+    </S.Container>
   );
 };
 
--- app/screens/Dashboard/styles.ts
+++ app/screens/Dashboard/styles.ts
@@ -1,11 +1,58 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
+
+  background-color: ${({ theme }) => theme.palette.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${({ theme }) => theme.rfValue(278)}px;
+  background-color: ${({ theme }) => theme.palette.blue};
+
   align-items: center;
 `;
 
-export const Text = styled.Text`
-  ${({ theme }) => theme.typography.body};
+export const InfoBar = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: ${({ theme }) => theme.rfValue(getStatusBarHeight() + 28)}px;
 `;
+
+export const User = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserPhoto = styled.Image`
+  width: ${({ theme }) => theme.rfValue(48)}px;
+  height: ${({ theme }) => theme.rfValue(48)}px;
+  border-radius: ${({ theme }) => theme.radius(5)};
+  margin-right: ${({ theme }) => theme.spacing(2)};
+`;
+
+export const UserInfo = styled.View``;
+
+export const UserGreetings = styled.Text`
+  font-family: ${({ theme }) => theme.typography.fonts.regular};
+  font-size: ${({ theme }) => theme.rfValue(18)}px;
+  color: ${({ theme }) => theme.palette.shape};
+`;
+
+export const UserName = styled.Text`
+  font-family: ${({ theme }) => theme.typography.fonts.bold};
+  font-size: ${({ theme }) => theme.rfValue(18)}px;
+  color: ${({ theme }) => theme.palette.shape};
+`;
+
+export const Icon = styled(Feather).attrs(({ theme }) => ({
+  name: 'power',
+  size: theme.rfValue(24),
+  color: theme.palette.orange,
+}))``;
